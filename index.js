74d36002a00a1208707c97cec71519d8f738f84d
Muhammad Jawad <akhterjawad566@gmail.com>
2024-11-14 16:22:21 +0500

    working

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+// import express from "express"
+// const app = express()
+// const port = 3000
+
+// // middleware
+// app.use(express.json())
+
+// app.get('/',(req,res)=>{
+// res.send('hello backend')
+
+// })
+
+// app.listen(port,()=>{console.log(port);
+// })
+
+
+
+
 import express from "express"
 const app = express()
 const port = 3000
 
+// middleware
+// middleware json ma convert karta ha
+app.use(express.json())
+
+let userArray = [];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('hello world')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+app.post('/user', (req, res) => {
+  const {
+    name
+  } = req.body
+  res.status(201).json({
+    name
+  })
+  userArray.push(name)
 })
+
+
+app.listen(port, () => { console.log(port) });
