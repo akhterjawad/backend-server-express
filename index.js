98e35e86059a52076cd5f37f0d61be34675ee99b
Muhammad Jawad <akhterjawad566@gmail.com>
2024-11-15 21:30:01 +0500

    working

--- index.js
+++ index.js
@@ -4,13 +4,17 @@
 
 // // middleware
 // app.use(express.json())
+// // middleware ka istemal hum karte hain jab humein backend par incoming JSON data ko parse karna hota hai. Jab bhi client (jaise frontend ya Postman) koi JSON data bhejta hai request body mein, to yeh middleware us data ko parse karke readable bana deta hai, taa ke aap req.body ke through us data ko access kar sako.
+// // yeh POST, PUT, aur PATCH methods ke sath zyada use hota hai, kyun ke in methods mein hum aksar data server ko bhejte hain (jaise form data ya koi JSON object).
 
-// app.get('/',(req,res)=>{
-// res.send('hello backend')
 
+// app.get('/', (req, res) => {
+//   res.send('hello backend')
+//   // Yeh sirf ek response bhej raha hai client ko, jo frontend ya Postman pe dikhega. Is case mein server client ko hello backend string return kar raha hai.
 // })
 
-// app.listen(port,()=>{console.log(port);
+// app.listen(port, () => {
+//   console.log(port);
 // })
 
 
@@ -21,8 +25,9 @@ const app = express()
 const port = 3000
 
 // middleware
-// middleware json ma convert karta ha
 app.use(express.json())
+// Yeh middleware ensure karta hai ke incoming JSON data ko parse kare, taa ke hum req.body ke zariye access kar saken.
+// Without express.json(), req.body undefined hoga.
 
 let userArray = [];
 
@@ -35,8 +40,11 @@ app.get('/', (req, res) => {
 app.post('/user', (req, res) => {
   const {
     name
-  } = req.body
-  res.status(201).json({
+  } = req.body //Yeh client ke bheje hue data ko represent karta hai (jo JSON format mein hota hai).
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+  res.status(201).json({ // Iska use tab hota hai jab koi naya resource (jaise user) successfully create ho.
     name
   })
   userArray.push(name)
