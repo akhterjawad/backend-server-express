f9052b68f894e3e718455e1899914ed151a5f57d
akhterjawad <akhterjawad566@gmail.com>
2024-11-27 18:34:10 +0500

    edit todo

--- 03-Full-Stack-Todo-App-With-MongoDB/server/src/controllers/todos.controllers.js
+++ 03-Full-Stack-Todo-App-With-MongoDB/server/src/controllers/todos.controllers.js
@@ -1,5 +1,6 @@
 import Todos from "../models/todos.models.js";
 import mongoose from "mongoose";
+
 // add todo
 const addTodo = (req, res) => {
   const { title, description } = req.body;
@@ -18,4 +19,83 @@ const addTodo = (req, res) => {
     message: "user added to database successfully",
   });
 };
-export { addTodo };
+
+// get all todo
+const getAllTodos = async (req, res) => {
+  const todos = await Todos.find({});
+  res.status(200).json({
+    todos: todos,
+  });
+};
+
+// get single todo
+const getTodoWithId = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid Id" });
+  }
+
+  const todo = await Todos.findById(id);
+  if (!todo) {
+    res.status(404).json({
+      message: "no todo found!",
+    });
+    return;
+  }
+  res.status(200).json(todo);
+};
+
+// delete todo
+const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid Id" });
+  }
+
+  const todo = await Todos.findOneAndDelete({ _id: id });
+  if (!todo) {
+    res.status(404).json({
+      message: "no todo found!",
+    });
+    return;
+  }
+
+  res.status(200).json({
+    message: "todo deleted successfully",
+    todo,
+  });
+};
+
+// edit todo
+const editTodo = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid Id" });
+  }
+  const { title, description } = req.body;
+  try {
+    let updatetodo = await Todos.findByIdAndUpdate({
+      _id: id,
+      title,
+      description,
+      new: true,
+    });
+    if (!title || !description) {
+      res.status(400).json({
+        message: "You must enter both fields",
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "todo edit successfully",
+      updatetodo,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { addTodo, getAllTodos, getTodoWithId, deleteTodo, editTodo };
--- 03-Full-Stack-Todo-App-With-MongoDB/server/src/routes/todos.routes.js
+++ 03-Full-Stack-Todo-App-With-MongoDB/server/src/routes/todos.routes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { addTodo } from "../controllers/todos.controllers.js";
+import {
+  addTodo,
+  getAllTodos,
+  getTodoWithId,
+  deleteTodo,
+  editTodo,
+} from "../controllers/todos.controllers.js";
 
 const router = express.Router();
 
 router.post("/todo", addTodo);
+router.get("/todos", getAllTodos);
+router.get("/todo/:id", getTodoWithId);
+router.delete("/todo/:id", deleteTodo);
+router.put("/todo/:id", editTodo);
 
 export default router;
